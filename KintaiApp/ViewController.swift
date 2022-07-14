8bd674cd17b6f029e5d35e5780a90738a60d7fe4
sugurunnba <sugurunnba7@gmail.com>
2022-07-14 23:18:30 +0900

    first commit

---
+++ KintaiApp/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  KintaiApp
+//
+//  Created by 高木克 on 2022/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var textField: UITextField!
+    
+    let hour: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+    let minutes: [Int] = [0, 15, 30, 45]
+    let list: [String] = ["メインクーン", "シャム", "ロシアンブルー", "アメリカンショートヘア", "ネベロング", "ビクシーボブ", "ラガマフィン", "ラパーマ"]
+        
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        label.text = "1.0"
+        self.pickerView.isHidden = true
+        
+        // 決定バーの生成
+       let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35))
+       let spacelItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+       let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+       toolbar.setItems([spacelItem, doneItem], animated: true)
+        
+       // インプットビュー設定
+       textField.inputView = pickerView
+       textField.inputAccessoryView = toolbar
+    
+       // デフォルト設定
+       pickerView.selectRow(2, inComponent: 0, animated: false)
+       
+    }
+    // 決定ボタン押下
+    @objc func done() {
+        textField.endEditing(true)
+        textField.text = "\(list[pickerView.selectedRow(inComponent: 0)])"
+    }
+
+    @IBAction func tapButton(_ sender: Any) {
+        pickerView.isHidden = false
+    }
+    
+    // UIPickerViewの列の数
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // UIPickerViewの行数、要素の全数
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return list.count
+    }
+    
+    // UIPickerViewに表示する配列
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return list[row]
+    }
+
+    // UIPickerViewのRowが選択された時の挙動
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        label.text = list[row]
+    }
+    
+}
+
