3122b5254f833de49a80bca7c97b319c9011481f
sugurunnba <sugurunnba7@gmail.com>
2022-07-21 23:42:17 +0900

    commit

--- KintaiApp/cellTableViewController.swift
+++ KintaiApp/cellTableViewController.swift
@@ -1,18 +1,22 @@
 //
-//  MainTableViewController.swift
+//  cellTableViewController.swift
 //  KintaiApp
 //
-//  Created by 高木克 on 2022/07/14.
+//  Created by 高木克 on 2022/07/17.
 //
 
 import UIKit
 
-class MainTableViewController: UITableViewController {
-    
-    let memos = ["1日", "2日", "3日"]
+class cellTableViewController: UITableViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var timeTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        timeTextField.delegate = self
+        //キーボードをtimePickerに変更
+        timeTextField.inputView = timePicker
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -20,32 +24,62 @@ class MainTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    //UIDatePickerをインスタンス化
+    let timePicker: UIDatePicker = {
+        let dp = UIDatePicker()
+        dp.datePickerMode = UIDatePicker.Mode.time
+        dp.timeZone = NSTimeZone.local
+        //時間をJapanese(24時間表記)に変更
+        dp.locale = Locale.init(identifier: "Japanese")
+        dp.addTarget(cellTableViewController.self, action: #selector(dateChange), for: .valueChanged)
+        //最小単位（分）を設定
+        dp.minuteInterval = 10
+        return dp
+    }()
+    @objc func dateChange(){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm"
+        timeTextField.text = "\(formatter.string(from: timePicker.date))"
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return memos.count
+        switch section {
+        case 0: // 「設定」のセクション
+          return 3
+        case 1: // 「その他」のセクション
+          return 1
+        default:
+          return 0
+        }
     }
+    
+//    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: String(describing: CustomeHeaderView.self))
+//            if let headerView = view as? CustomeHeaderView {
+//                headerView.someFunction()
+//                return headerView
+//            }
+//            return nil
+//    }
 
+    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
-        let memo = memos[indexPath.row]
-        let dateLabel = cell.viewWithTag(1) as! UILabel
-        dateLabel.text = memo
-//        let hourTextField = cell.viewWithTag(1) as! UITextField
-//        hourTextField.text = memo
-        
-//        let minutesTextField = cell.viewWithTag(2) as! UITextField
-//        minutesTextField.text = memo
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
 
         return cell
     }
+    */
 
     /*
     // Override to support conditional editing of the table view.
