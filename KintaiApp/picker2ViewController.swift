f84ca12289a9e89b39dc1b74dfdcbe5fef808d85
sugurunnba <sugurunnba7@gmail.com>
2022-07-27 22:33:38 +0900

    pickerViewの実装(表示だけ、下からニュッとはまだできていない)

--- KintaiApp/picker2ViewController.swift
+++ KintaiApp/picker2ViewController.swift
@@ -7,20 +7,9 @@
 
 import UIKit
 
-class picker2ViewController: UIViewController,UITextFieldDelegate,UIPickerViewDelegate, UIPickerViewDataSource, UITableViewDelegate {
-
-    //textFieldとのアウトレット接続
-//    @IBOutlet weak var kanaTextField: UITextField!
-//    @IBOutlet weak var dateTextField: UITextField!
-//    @IBOutlet weak var numberTextField: UITextField!
+class picker2ViewController: UIViewController,UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var tableView: UITableView!
-    //pickerViewのプロパティを作る
-    var kanaPickerView : UIPickerView = UIPickerView()
-    var numberPickerView  : UIPickerView = UIPickerView()
-    //datePickerのプロパティを作る
-    var datePickerView  :UIDatePicker = UIDatePicker()
-    
+    @IBOutlet weak var tableView: UITableView!    
     //pickerViewに表示する配列を作成
     let list1:[String] = ["10","に","さん","よん","ご",]
     let list2:[String] = ["45","2","3","4","5","6","7"]
@@ -28,15 +17,13 @@ class picker2ViewController: UIViewController,UITextFieldDelegate,UIPickerViewDe
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UINib(nibName: "sampleTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
-        makePickerKeybord()
     }
 
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         list1.count
     }
     
-    func tableView(tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! sampleTableViewCell
 //        if indexPath.section == 0 {
 //            cell.textLabel!.text = times[indexPath.row]
@@ -48,77 +35,9 @@ class picker2ViewController: UIViewController,UITextFieldDelegate,UIPickerViewDe
         cell.kanaTextField.text = "あ"
         cell.dateTextField.text = "i"
         cell.numberTextField.text = "う"
+        cell.makePickerKeybord(cellce: cell)
         return cell
     }
-    
-    //キーボードの設定
-    func makePickerKeybord(){
-        //かな数字のPickerviewをキーボードにする設定
-        kanaPickerView.tag = 1
-        kanaPickerView.delegate = self
-        kanaTextField.inputView = kanaPickerView
-        kanaTextField.delegate = self
-        
-        //numberのPickerviewをキーボードにする設定
-        numberPickerView.tag = 2
-        numberPickerView.delegate = self
-        numberTextField.inputView = numberPickerView
-        numberTextField.delegate = self
-        
-        // 日付ピッカーをキーボードにする設定
-        datePickerView.datePickerMode = UIDatePicker.Mode.dateAndTime
-        datePickerView.timeZone = TimeZone(identifier: "Asia/Tokyo")
-        datePickerView.locale = Locale(identifier: "ja_JP")
-        datePickerView.addTarget(self, action: Selector(("dateChange")), for: .valueChanged)
-        dateTextField.inputView = datePickerView
-        dateTextField.delegate = self
-    }
-    
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        switch pickerView.tag {
-        case 1:
-            return list1.count
-        case 2:
-            return list2.count
-        default:
-            return 0
-        }
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        switch pickerView.tag {
-            case 1:
-                return list1[row]
-            case 2:
-                return list2[row]
-        default:
-            return "error"
-        }
-    }
-    
-     //pickerが選択された際に呼ばれるデリゲートメソッド.
-     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-         switch pickerView.tag {
-             case 1:
-                 return kanaTextField.text = list1[row]
-             case 2:
-                 return numberTextField.text = list2[row]
-         default:
-             return
-         }
-     }
-     
-     //datePickerが変化すると呼ばれる
-    @objc func dateChange(){
-         let formatter = DateFormatter()
-         formatter.dateFormat = "HH:mm"
-         dateTextField.text = "\(formatter.string(from: datePickerView.date))"
-     }
 }
 
 // MARK: - キーボードにと閉じるボタンを付ける
--- KintaiApp/sampleTableViewCell.swift
+++ KintaiApp/sampleTableViewCell.swift
@@ -7,11 +7,43 @@
 
 import UIKit
 
-class sampleTableViewCell: UITableViewCell {
-
+class sampleTableViewCell: UITableViewCell ,UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate{
+    
     @IBOutlet weak var kanaTextField: UITextField!
     @IBOutlet weak var dateTextField: UITextField!
-    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!    //pickerViewに表示する配列を作成
+    
+    //pickerViewのプロパティを作る
+    var kanaPickerView : UIPickerView = UIPickerView()
+    var numberPickerView  : UIPickerView = UIPickerView()
+    //datePickerのプロパティを作る
+    var datePickerView  :UIDatePicker = UIDatePicker()
+    
+    let list1:[String] = ["10","に","さん","よん","ご",]
+    let list2:[String] = ["45","2","3","4","5","6","7"]
+    
+    //キーボードの設定
+    func makePickerKeybord(cellce: sampleTableViewCell){
+        //かな数字のPickerviewをキーボードにする設定
+        kanaPickerView.tag = 1
+        kanaPickerView.delegate = self
+        cellce.kanaTextField.inputView = kanaPickerView
+        cellce.kanaTextField.delegate = self
+        
+        //numberのPickerviewをキーボードにする設定
+        numberPickerView.tag = 2
+        numberPickerView.delegate = self
+        cellce.numberTextField.inputView = numberPickerView
+        cellce.numberTextField.delegate = self
+        
+        // 日付ピッカーをキーボードにする設定
+        datePickerView.datePickerMode = UIDatePicker.Mode.dateAndTime
+        datePickerView.timeZone = TimeZone(identifier: "Asia/Tokyo")
+        datePickerView.locale = Locale(identifier: "ja_JP")
+        datePickerView.addTarget(self, action: Selector(("dateChange")), for: .valueChanged)
+        cellce.dateTextField.inputView = datePickerView
+        cellce.dateTextField.delegate = self
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -24,4 +56,48 @@ class sampleTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView.tag {
+        case 1:
+            return list1.count
+        case 2:
+            return list2.count
+        default:
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+            case 1:
+                return list1[row]
+            case 2:
+                return list2[row]
+        default:
+            return "error"
+        }
+    }
+    
+     //pickerが選択された際に呼ばれるデリゲートメソッド.
+     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+         switch pickerView.tag {
+             case 1:
+                 return kanaTextField.text = list1[row]
+             case 2:
+                 return numberTextField.text = list2[row]
+         default:
+             return
+         }
+     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //datePickerが変化すると呼ばれる
+    @objc func dateChange(){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        dateTextField.text = "\(formatter.string(from: datePickerView.date))"
+    }
 }
